70025246449b2ec0fdce507f9b59bfd93b089a2d
QodirovaNozima1 <nozimaqodirova07@gmail.com>
2024-06-05 01:12:41 +0500

    work

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// { 
+//     // masala -1
+// let a = prompt("a soni  kiritng")
+// let p = 4 * a
+// alert(`perimetri: ${p}`)
+// }
+
+// {
+//     //    masala -2
+//     let a = prompt("a sonini kiriting")
+//     let s = a * a
+//     alert(`yuzasi: ${s}`)
+// }
+
+// {
+//     //masala -3
+//     let a = +prompt("a son kirting")
+//     let b = +prompt("b son kiritng")
+//     let s = (a * b)
+//     alert(`Yuzasi: ${s}`)
+//     let p = 2 * (a + b)
+//     alert(`perimetri ${p}`)
+// }
+
+// {
+//     //masala -4
+//     let d = +prompt("d son kiriting")
+//     let L = 3.14 * d
+//     alert(`uzunligi ${L}`)
+// }
+
+// {
+//     //masala -5
+//     let a = prompt("a son kiriting")
+//     let v = a * a *a
+//     alert(`Kubning hajmi ${v}`)
+//     let s = 6 * (a * a)
+//     alert(`Kubning to'la sirti ${s}`)
+// }
+
+// {
+//     //masala -6
+//     let a = +prompt("a son kiriting")
+//     let b = +prompt("b son kiriting")
+//     let c = +prompt("c son kiriting")
+//     let v = a * b * c
+//     alert(`paralelepipedning hajmi ${v}`)
+//     let s = 2 * (a * b + b * c + a * c)
+//     alert(`paralelepipedning to'la sirti ${s}`)
+// }
+
+// {
+//     //masala -7
+//     let R = +prompt("doiraning radiusini kiriting")
+//     let L = 2 * 3.14 * R
+//     alert(`Uzunligi ${L}`)
+//     let s = 3.14 * ( R * R)
+//     alert(`Yuzasi ${s}`)
+// }
+
+{
+    //masala -8
+    let a = +prompt("a son kiriting")
+    let b = +prompt("b son kiriting")
+    let n = (a + b)/2
+    alert(`o'rta arifmetigi ${n}`)
+}
